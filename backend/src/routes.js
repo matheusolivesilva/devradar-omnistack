3f81a28c77aadd3e7ec0423104c1fecb2237666d
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-01-16 01:40:19 -0300

    Change routes.js, preparing to receive data from Github API, and after to storing Data into MongoDB

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
-
+const axios = require('axios');
 const routes = Router();
+const Dev = require('./models/Dev');
 
 
-routes.post('/devs/', (request, response) => {
-    console.log(request.body);
-    return response.json({message: 'Hello oministack'});
+routes.post('/devs/', async (request, response) => {
+    const { github_username, techs } = request.body;
+    const apiResponse = await  axios.get(`https://api.github.com/users/${github_username}`);
+    const { name = login, avatar_url, bio } = apiResponse.data;
+    const techsArray = techs.split(',').map(tech => tech.trim());
+    const dev = await Dev.create({
+        github_username: github_username,
+        name: name,
+        avatar_url,
+        bio,
+        techs: techsArray
+    });
+    return response.json(dev);
 });
 
 module.exports = routes;
