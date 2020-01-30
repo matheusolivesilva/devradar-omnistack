0258f0a77520667d4693682e8eeb07a7bc1ae808
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-01-29 23:31:49 -0300

    Change Main.js, using stylesheet

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import MapView from 'react-native-maps';
+import { StyleSheet } from 'react-native';
 
 function Main() {
-    return <MapView style={{ flex: 1 }} />
+    return <MapView style={styles.map} />
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+})
 
 export default Main;
