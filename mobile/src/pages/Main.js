3da528c06e90941ae45ef945421b5743e33fa846
Matheus Oliveira <matheus.oliveira.silv@gmail.com>
2020-01-31 14:55:11 -0200

    Change Main.js, using callout when the image is clicked

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 function Main() {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -32,6 +32,13 @@ function Main() {
     <MapView initialRegion={currentRegion} style={styles.map} >
         <Marker coordinate ={{ latitude: -23.5941774, longitude: -46.5811725 }} >
             <Image  style={styles.avatar} source={{ uri:  'https://avatars2.githubusercontent.com/u/19143379?s=460&v=4' }} />
+            <Callout>
+                 <View style={styles.callout}>
+                    <Text style={styles.devName}>Matheus Oliveira da Silva</Text>
+                    <Text style={styles.devBio}>Hi! I'm a brazilian hobbyist of software! Develop and design softwares is my passion!</Text>
+                    <Text style={styles.devTechs}>PHP, JAVASCRIPT</Text>
+                 </View>
+            </Callout>
         </Marker>
     </MapView>
     );
@@ -47,8 +54,27 @@ const styles = StyleSheet.create({
         height: 54,
         borderRadius: 4,
         borderWidth: 4,
-        borderColor: '#fff'
+        borderColor: '#fff',
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
     }
+
 })
 
 export default Main;
