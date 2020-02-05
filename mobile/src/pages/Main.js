40f2508985c7c1a17eae0b20e8b843f0e480e1b4
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-02-05 02:19:00 -0300

    Change Mains.js and Profile.js, search form and 'redirect to profile' page

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect} from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
     useEffect(() => {
@@ -29,20 +30,36 @@ function Main({ navigation }) {
         return null;
     }
     return  (
-    <MapView initialRegion={currentRegion} style={styles.map} >
-        <Marker coordinate ={{ latitude: -23.5941774, longitude: -46.5811725 }} >
-            <Image  style={styles.avatar} source={{ uri:  'https://avatars2.githubusercontent.com/u/19143379?s=460&v=4' }} />
-            <Callout onPress={() => {
-                navigation.navigate('Profile', { github_username: 'dialog'})
-            }}>
-                 <View style={styles.callout}>
-                    <Text style={styles.devName}>Matheus Oliveira da Silva</Text>
-                    <Text style={styles.devBio}>Hi! I'm a brazilian hobbyist of software! Develop and design softwares is my passion!</Text>
-                    <Text style={styles.devTechs}>PHP, JAVASCRIPT</Text>
-                 </View>
-            </Callout>
-        </Marker>
-    </MapView>
+    <>
+        <MapView initialRegion={currentRegion} style={styles.map} >
+            <Marker coordinate ={{ latitude: -23.5941774, longitude: -46.5811725 }} >
+                <Image  style={styles.avatar} source={{ uri:  'https://avatars2.githubusercontent.com/u/19143379?s=460&v=4' }} />
+                <Callout onPress={() => {
+                    navigation.navigate('Profile', { github_username: 'dialog'})
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Matheus Oliveira da Silva</Text>
+                        <Text style={styles.devBio}>Hi! I'm a brazilian hobbyist of software! Develop and design softwares is my passion!</Text>
+                        <Text style={styles.devTechs}>PHP, JAVASCRIPT</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+
+        <View style={ styles.searchForm }>
+            <TextInput 
+                style= { styles.searchInput }
+                placeholder='Buscar devs por techs...'
+                placeholderTextColor='#999'
+                autoCapitalize='words'
+                autoCorrect={false}
+            />
+
+            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <MaterialIcons name="my-location" size={20} color='#fff'/>
+            </TouchableOpacity>
+        </View>
+    </>
     );
 }
 
@@ -75,8 +92,43 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
-    }
+    },
 
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: { 
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
 })
 
 export default Main;
